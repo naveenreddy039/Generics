f2a66042e9ac4133a9262af92b0792b51cbf6d70
naveen <naveenreddy.k07@gmail.com>
2021-11-17 18:04:35 +0530

    first commit

---
+++ MaxIntegerUsingGenerics/MaxNumberCheck.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxIntegerUsingGenerics
+{
+    public class MaxNumberCheck
+    {
+        public static int MaxIntegerNumber(int firstValue, int SecondValue, int thirdValue)
+        {
+            if (firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (SecondValue.CompareTo(thirdValue) > 0 && SecondValue.CompareTo(firstValue) > 0)
+            {
+                return SecondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(SecondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue,secondValue,thirdValue");
+        }
+        public static double MaxIntegerNumber(double firstValue, double SecondValue, double thirdValue)
+        {
+            if (firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (SecondValue.CompareTo(thirdValue) > 0 && SecondValue.CompareTo(firstValue) > 0)
+            {
+                return SecondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(SecondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue,secondValue,thirdValue");
+        }
+        public static string MaxIntegerNumber(string firstString, string SecondString, string thirdString)
+        {
+            if (firstString.CompareTo(SecondString) > 0 && firstString.CompareTo(thirdString) > 0)
+            {
+                return firstString;
+            }
+            if (SecondString.CompareTo(thirdString) > 0 && SecondString.CompareTo(firstString) > 0)
+            {
+                return SecondString;
+            }
+            if (thirdString.CompareTo(firstString) > 0 && thirdString.CompareTo(SecondString) > 0)
+            {
+                return thirdString;
+            }
+            throw new Exception("firstValue,secondValue,thirdValue");
+        }
+    }
+}
---
+++ MaxIntegerUsingGenerics/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MaxIntegerUsingGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("first max number:");
+            int output = MaxNumberCheck.MaxIntegerNumber(22, 33, 11);
+            Console.WriteLine(output);
+
+            Console.WriteLine("second max floatNumber:");
+            double output2 = MaxNumberCheck.MaxIntegerNumber(11.1, 22.2, 33.3);
+            Console.WriteLine(output2);
+
+            Console.WriteLine("first max string:");
+            string output3 = MaxNumberCheck.MaxIntegerNumber("hello","reddy","naveen");
+            Console.WriteLine(output3);
+        }
+    }
+}
